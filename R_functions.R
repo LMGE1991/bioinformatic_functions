66b65acdd212caa80734fdbb247ebc8771cba544
LMGE1991 <129909957+LMGE1991@users.noreply.github.com>
2024-04-30 18:10:36 +1000

    Update R_functions.R

--- R_functions.R
+++ R_functions.R
@@ -125,4 +125,31 @@ calculate_MP <- function(genotype_data){
 MP_index <- do.call(rbind, lapply(split_data_sample, calculate_MP))
 
 
+## The function 'panel.cor' and 'panel.hist' described here 
+## are designed to be used as a custom panel functions in a pairs plot 
+## (e.g., created with the pairs() function in R). 
+## Its primary purpose is to display the correlation coefficient between two variables in the cells of the upper or lower triangle of a pairs plot matrix.
+
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...) { # x and y are the pairs of variables for which the correlation will be calculated and displayed. 'cex.cor' is a character expansion factor for the correlation coefficient text, determining its size. If it's not provided, it will be calculated based on the text width.
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y, use = "complete.obs"))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste(prefix, txt, sep = "")
+  if (missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex =  cex.cor * (1 + r) / 2)
+}
+
+panel.hist <- function(x, ...) {
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks
+  nB <- length(breaks)
+  y <- h$counts
+  y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col = "white", ...)
+}
 
